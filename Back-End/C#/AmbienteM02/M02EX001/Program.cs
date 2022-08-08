d536953141ff05a0e0ea0b7100c74d72889bced3
João <joaoezeki@gmail.com>
2022-08-08 17:58:46 -0300

    C# M02

---
+++ Back-End/C#/AmbienteM02/M02EX001/Program.cs
@@ -0,0 +1,26 @@
+﻿// Gerando numeros aleatorios
+Random gerador = new Random();
+int op1 = gerador.Next(1, 101);
+int op2 = gerador.Next(1, 101);
+
+Console.WriteLine(op1);
+Console.WriteLine(op2);
+
+Console.WriteLine();
+Console.WriteLine();
+
+// Realizando as operações unarias
+Console.WriteLine($"Operador de identidade:          +{op1} = +{op1}");
+Console.WriteLine($"Operadoir de nagação aritmética: -{op2} = -{op2}");
+
+Console.WriteLine();
+
+// Realizando as operações binarias
+Console.WriteLine($"Operação de adção:                    {op1} + {op2} = {op1 + op2}");
+Console.WriteLine($"Operação de subtração:                {op1} - {op2} = {op1 - op2}");
+Console.WriteLine($"Operação de multiplicação:            {op1} * {op2} = {op1 * op2}");
+Console.WriteLine($"Operação de divição inteira:          {op1} / {op2} = {op1 / op2}");
+Console.WriteLine($"Operação de resto da divisão inteira: {op1} % {op2} = {op1 % op2}");
+
+
+Console.ReadKey();
---
+++ Back-End/C#/AmbienteM02/M02EX002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Gerando numeros aleatorios
+Random gerador = new Random();
+int op1 = gerador.Next(1, 101);
+int op2 = gerador.Next(1, 101);
+
+float op3 = gerador.Next(1, 51);
+
+Console.WriteLine(op1);
+Console.WriteLine(op2);
+Console.WriteLine(op3);
+
+Console.WriteLine();
+Console.WriteLine();
+
+// Divisão inteira e Divisão real
+Console.WriteLine($"Operação de divição inteira: {op1} / {op2} = {op1 / op2}");
+Console.WriteLine($"Operação de divição real:    {op3} / {op2} = {op3 / op2}");
+
+Console.ReadKey();
