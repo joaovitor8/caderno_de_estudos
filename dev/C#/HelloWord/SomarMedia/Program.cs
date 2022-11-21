b4b912ee3d839fda373961d3c331a6899e379800
João Vitor Ezequiel <joaoezeki@gmail.com>
2022-11-21 20:24:23 -0300

    Calculadora C#

---
+++ dev/C#/HelloWord/SomarMedia/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomarMedia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
