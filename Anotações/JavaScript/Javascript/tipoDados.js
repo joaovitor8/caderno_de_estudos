5aa2338599bbd352c3610203a0b51c341fd834d9
João Vitor Ezequiel <joaoezeki@gmail.com>
2023-04-17 17:35:05 -0300

    .

---
+++ Anotações/JavaScript/Javascript/tipoDados.js
@@ -0,0 +1,49 @@
+// typeof - Conferir qual é o Tipo Primitivo de uma Variavel
+
+// String  -  Cadeia de caracteres (Texto)
+//   Aspas duplas
+//   Aspas simples
+
+
+// Number  -  Números
+//   Inteiros
+//   Float
+//   Not a Number
+//   Infinity
+
+
+// Boolean  -  Somente 2 valores
+//   true
+//   false
+
+
+// undefined  -  indefinido
+
+
+// null  -  nulo
+//   objeto que não possui nada dentro
+//   diferente de indefinito
+
+
+// Array  -  Agrupamento de dados
+//    EX:  const pessoas = ['João', 'Flavia', 'Mateus']
+//   Acessando
+//     console.log(pessoas[2])
+//   push() - Adiciona no array
+//   pop() - Tira o ultimo valor
+//   shift() - Tira o primeiro valor
+//   forEach() -
+//   filter() - fitra elementos de uma lista
+//   reduce() - reduz os elementos de uma lista a um uinico elemento
+
+
+// Object  -  Objeto
+//   Propriedade / Atributos
+//   Funcionalidade / Métodos
+//     EX:  const pessoa = {nome: "João", idade: 20, peso: 50}
+//   Acessando
+//     console.log(pessoa.nome)
+
+//   JSON
+//     stringify  -  transforma o objeto tudo para texto  string
+//     parce  -  tranforma em objeto
