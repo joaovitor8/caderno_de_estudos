d05e8ef3bb4f6771a828708058c73e813fdddfd1
João Vitor Ezequiel <joaoezeki@gmail.com>
2023-04-18 20:39:58 -0300

    .

--- Anotações/JavaScript/Javascript/variavel.js
+++
@@ -1,7 +0,0 @@
-//var - global, todo mundo pode acessar
-//let - apenas quem esta no mesmo escopo pode acessar
-//const - imutavel, apos passar um valor não podera muda-lo
-
-var nomeVariavelVar = 'Um dado'
-let nomeVariavelLet = 'Um dado'
-const nomeVariavelConst = 'Um dado'
